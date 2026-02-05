746ce343b6c07d57e58dcf9911effec718bd0a4a
mohammadrasoulsalmani <mrsa1380moit@gmail.com>
2026-02-05 10:51:13 -0800

    update the models by base model function and final figure 8

--- src/models.py
+++ src/models.py
@@ -1,233 +1,118 @@
-from transformers import AutoModel
-import torch.nn as nn
+"""
+Models from the original paper
+Copy this from your models.ipynb notebook
+"""
+
 import torch
+import torch.nn as nn
+import numpy as np
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+from collections import defaultdict
+import pickle
+import tqdm
 
-class UserNetwork(nn.Module):
-    """
-    3 layer network
-    """
+class BinnedSeqDataset(Dataset):
+    """Dataset class matching the original implementation"""
+    def __init__(self, sequences, text_type=1):
+        self.sequences = sequences
+        self.text_type = text_type
     
-    def __init__(self,input_shape=2799,l1_h=1024,dropout=0.1,activation="relu"):
-        """
-        """
-        super(UserNetwork, self).__init__()
+    def __len__(self):
+        return len(self.sequences)
+    
+    def __gen_text_rep__(self, text_dict):
+        if self.text_type == 1:
+            return text_dict["mean"]
+        if self.text_type == 2:
+            return torch.concat([text_dict["cls"], text_dict["mean"]], dim=-1)
+    
+    def __getitem__(self, idx):
+        obs_feats = self.sequences[idx]
         
-        self.params_ = {"input_shape":input_shape,
-                       "l1_h":l1_h,
-                        "dropout":dropout,
-                        "activation":activation}
+        res_feats = {}
+        non_engage_rep = None
+        engage_rep = None
         
-        l2_h = int(l1_h/2)
-        l3_h = int(l2_h/2)
+        for k in obs_feats.keys():
+            if ((k not in ["date_ranges", "username"]) and 
+                ("text" not in k) and ("top" not in k)):
+                
+                if np.isscalar(obs_feats[k]):
+                    res_feats[k] = torch.Tensor(np.array(obs_feats[k]).reshape(1, -1)).squeeze()
+                else:
+                    res_feats[k] = torch.Tensor(obs_feats[k])
+            
+            if "text" in k and str(self.text_type) in k:
+                if "nengagement" in k:
+                    non_engage_rep = self.__gen_text_rep__(obs_feats[k])
+                else:
+                    engage_rep = self.__gen_text_rep__(obs_feats[k])
         
-        self.fc1 = nn.Linear(input_shape,l1_h)
-        # self.fc2 = nn.Linear(l1_h,l2_h)
-        self.fc4 = nn.Linear(l1_h,1)
+        res_feats["text_feat"] = torch.concat([engage_rep, non_engage_rep], dim=-1)
+        res_feats["date_ranges"] = [(x[0].strftime('%Y-%m-%d'), x[1].strftime('%Y-%m-%d')) 
+                                   for x in obs_feats["date_ranges"]]
+        res_feats["username"] = list(set(obs_feats["username"]))
+        res_feats["hashtag_feat"] = obs_feats["tophashes"]
+        res_feats["mentions_feat"] = obs_feats["topmentions"]
         
-        self.dropout = nn.Dropout(dropout)
+        return res_feats
+
+class LSTMForecasterMF(nn.Module):
+    """Multiple Feature Network - The main model from the paper"""
+    
+    def __init__(self, input_size, hidden_dim_lstm=64, hidden_text=128, 
+                 hidden_inter=128, num_layers=1, bidirectional=False, 
+                 dropout=0.1, output_size=7, activation="sigmoid"):
+        super(LSTMForecasterMF, self).__init__()
         
-        self.activation = torch.relu
+        self.num_layers = num_layers
+        self.num_directions = 2 if bidirectional else 1
+        
+        self.lstm = nn.LSTM(
+            input_size=input_size + hidden_text + 4,
+            hidden_size=hidden_dim_lstm,
+            num_layers=num_layers,
+            bidirectional=bidirectional,
+            batch_first=True
+        )
         
         if activation == "relu":
-            self.activation = torch.relu
+            self.activation = nn.ReLU()
+        else:
+            self.activation = nn.Sigmoid()
         
-        if activation == "sigmoid":
-            self.activation = torch.sigmoid
-        
+        self.dropout = nn.Dropout(p=dropout)
+        self.fc_text = nn.Linear(3072, hidden_text)
+        self.fc_inter = nn.Linear(
+            int(hidden_dim_lstm * self.num_directions) + 4 + 1536, 
+            hidden_inter
+        )
+        self.fc_out = nn.Linear(hidden_inter, output_size)
     
-    def forward(self,x):
-        """
-        """
-        x_1 = self.activation(self.fc1(x))
-        x_1 = self.dropout(x_1)
-        # x_2 = self.activation(self.fc2(x_1))
-        # x_2 = self.dropout(x_2)
-        x_4 = torch.sigmoid(self.fc4(x_1))
-        return x_1, x_4 
-    
-
-class TextNetwork(nn.Module):
-    """
-    Uses BertTweet
-    """
-    
-    def __init__(self,input_shape=50,lh=128,freeze_bert=True,dropout=0.1,pre_trained="cardiff",pooling="mean",activation="relu"):
-        """
-        """
-        super(TextNetwork, self).__init__()
+    def forward(self, count_feats, in_time_feats, out_time_feats, 
+                ns_feats, mention_feats, hash_feats, text_feats):
         
-        self.params_ = {"input_shape":input_shape,
-                       "lh":lh,
-                        "dropout":dropout,
-                        "activation":activation,
-                        "freeze_bert":freeze_bert,
-                        "pre_trained":pre_trained,
-                        "pooling":pooling}
+        self.lstm.flatten_parameters()
         
-        self.bert_tweet = None
-        self.dropout = nn.Dropout(dropout)
+        text_feats = self.fc_text(text_feats)
+        feats = torch.concat([count_feats, text_feats, in_time_feats], dim=-1)
         
-        if pre_trained == "vinai":
+        batch_size = feats.shape[0]
+        out, (ht, ct) = self.lstm(feats)
         
-            self.bert_tweet = AutoModel.from_pretrained("vinai/bertweet-base")
+        ht_reshaped = torch.reshape(ht, (self.num_layers, self.num_directions, batch_size, ht.shape[-1]))
+        ht_final_layer = ht_reshaped[-1, :, :, :]
         
-        if pre_trained == "cardiff":
+        if self.num_directions > 1:
+            ht_forward = ht_final_layer[0, :, :]
+            ht_backward = ht_final_layer[1, :, :]
+            ht_final = torch.concat([ht_forward, ht_backward], dim=-1)
+        else:
+            ht_final = ht_final_layer[-1, :, :]
         
-            self.bert_tweet = AutoModel.from_pretrained("cardiffnlp/twitter-roberta-base")
+        inter_feats = torch.concat([ht_final, out_time_feats, hash_feats], dim=-1)
+        fc_inter = self.activation(self.fc_inter(inter_feats))
+        fc_out = self.fc_out(fc_inter)
         
-        
-        if freeze_bert:
-            for param in self.bert_tweet.parameters():
-                param.requires_grad = False
-        
-        self.pooling = pooling
-        
-        self.fc_in = nn.Linear(768,lh)
-        
-        if self.pooling == "concat":
-            self.fc_in = nn.Linear(768*2,lh)
-            
-        self.fc_out = nn.Linear(lh,1)
-        
-        self.activation = torch.relu
-        
-        if activation == "relu":
-            self.activation = torch.relu
-        
-        if activation == "sigmoid":
-            self.activation = torch.sigmoid
-    
-    def forward(self,x):
-        """
-        """
-        embeds = self.bert_tweet(x) # returns batch_size x max_length x 768
-        
-        # pool across max_length
-        
-        if self.pooling == "mean":
-            embeds = torch.mean(embeds.last_hidden_state,axis=1) # batch_size x 768
-        
-        if self.pooling == "max":
-            embeds, max_indices  = torch.max(embeds.last_hidden_state,axis=1)
-        
-        if self.pooling == "concat":
-            
-            mean_ = torch.mean(embeds.last_hidden_state,axis=1)
-            max_,max_indices = torch.max(embeds.last_hidden_state,axis=1)
-            
-            embeds = torch.concat((max_,mean_),axis=1)
-        
-        if self.pooling == "special":
-            
-            embeds = embeds.last_hidden_state[:,0,:]
-        
-        x_1 = self.activation(self.fc_in(embeds))
-        x_1 = self.dropout(x_1)
-        x_2 = torch.sigmoid(self.fc_out(x_1))
-        
-        return x_1, x_2 
-    
-class CombinedNetwork(nn.Module):
-
-    
-    """
-    This network uses user and text features toghether
-    """
-    def __init__(self,user_input_shape=26,text_lh=128,user_lh=64,freeze_bert=True,dropout=0.1,pre_trained="cardiff",pooling="mean",user_activation="relu",text_activation="sigmoid"):
-        """
-        """
-        super(CombinedNetwork, self).__init__()
-        
-        self.params_ = {"user_input_shape":user_input_shape,
-                       "text_lh":text_lh,
-                        "dropout":dropout,
-                        "user_lh":user_lh,
-                        "freeze_bert":freeze_bert,
-                        "pre_trained":pre_trained,
-                        "pooling":pooling,
-                        "user_activation":user_activation,"text_activation":text_activation}
-        
-        self.bert_tweet = None
-        self.dropout = nn.Dropout(dropout)
-        
-        if pre_trained == "vinai":
-        
-            self.bert_tweet = AutoModel.from_pretrained("vinai/bertweet-base")
-        
-        if pre_trained == "cardiff":
-        
-            self.bert_tweet = AutoModel.from_pretrained("cardiffnlp/twitter-roberta-base")
-        
-        
-        if freeze_bert:
-            for param in self.bert_tweet.parameters():
-                param.requires_grad = False
-        
-        self.pooling = pooling
-        
-        self.fc_text_in = nn.Linear(768,text_lh)
-        
-        if self.pooling == "concat":
-            self.fc_text_in = nn.Linear(768*2,text_lh)
-        
-        self.text_activation = torch.relu
-        
-        if text_activation == "relu":
-            self.text_activation = torch.relu
-        
-        if text_activation == "sigmoid":
-            self.text_activation = torch.sigmoid
-        
-        self.user_activation = torch.relu
-        
-        if user_activation == "relu":
-            self.user_activation = torch.relu
-        
-        if user_activation == "sigmoid":
-            self.user_activation = torch.sigmoid
-        
-        
-        self.fc_user_in = nn.Linear(user_input_shape,user_lh)
-        
-        self.fc_out = nn.Linear(user_lh+text_lh,1)
-        
-        
-    def forward(self,x_user,x_text):
-        """
-        """
-        # text representations and pooling
-        
-        text_embeds = self.bert_tweet(x_text)
-        
-        if self.pooling == "mean":
-            pooled_embeds = torch.mean(text_embeds.last_hidden_state,axis=1) # batch_size x 768
-        
-        if self.pooling == "max":
-            pooled_embeds, max_indices  = torch.max(text_embeds.last_hidden_state,axis=1)
-        
-        if self.pooling == "concat":
-            
-            mean_ = torch.mean(text_embeds.last_hidden_state,axis=1)
-            max_,max_indices = torch.max(text_embeds.last_hidden_state,axis=1)
-            
-            pooled_embeds = torch.concat((max_,mean_),axis=1)
-        
-        if self.pooling == "special":
-            
-            pooled_embeds = text_embeds.last_hidden_state[:,0,:]
-        
-        text_inter = self.text_activation(self.fc_text_in(pooled_embeds))
-        
-        text_inter = self.dropout(text_inter)
-        
-        user_inter = self.user_activation(self.fc_user_in(x_user))
-        
-        user_inter = self.dropout(user_inter)
-        
-        concat_rep = torch.concat((user_inter,text_inter),axis=1)
-        
-        out = self.fc_out(concat_rep)
-        
-        out = torch.sigmoid(out) 
-        
-        return concat_rep, out
+        return fc_inter, fc_out
